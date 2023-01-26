c9e2f4cdb6556202517cfcd87b20950163b8b9f4
bs0462 <bs0462@Sandeep-IT.BANKIT.LAN>
2023-01-26 22:54:16 +0530

    first commit

---
+++ SpringSecurityCardApp/src/main/java/com/card/SpringSecurityCardApp/SpringSecurityCardAppApplication.java
@@ -0,0 +1,13 @@
+package com.card.SpringSecurityCardApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityCardAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityCardAppApplication.class, args);
+	}
+
+}
---
+++ SpringSecurityCardApp/src/test/java/com/card/SpringSecurityCardApp/SpringSecurityCardAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.card.SpringSecurityCardApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityCardAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
